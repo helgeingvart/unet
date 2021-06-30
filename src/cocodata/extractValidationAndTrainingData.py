23380e0eb886da45ce719593e90652046f08d08c
Helge Fredriksen <Helge.Fredriksen@uit.no>
2021-06-30 14:20:56 +0200

    This version of the script produces categorical data input for training av validation

--- src/cocodata/extractValidationAndTrainingData.py
+++ src/cocodata/extractValidationAndTrainingData.py
@@ -5,58 +5,91 @@ from PIL import Image as im
 import shutil
 
 dataDir = '/home/helge/data/coco'
-srcDir= os.path.join(dataDir, 'images')
+srcDir = os.path.join(dataDir, 'images')
 train = os.path.join(srcDir, 'train2017')
 validate = os.path.join(srcDir, 'val2017')
 test = os.path.join(srcDir, 'test2017')
 
 dstDir = '/home/helge/dev/unet/data/coco'
-trainDir = os.path.join(dstDir,'train')
-trainMaskDir = os.path.join(trainDir,'mask')
-trainImgDir = os.path.join(trainDir,'image')
-validateDir = os.path.join(dstDir,'validate')
-valImgDir = os.path.join(validateDir,'image')
-valMaskDir = os.path.join(validateDir,'mask')
-testDir = os.path.join(dstDir,'test/input')
-resultDir = os.path.join(dstDir,'result')
+trainDir = os.path.join(dstDir, 'train')
+validateDir = os.path.join(dstDir, 'validate')
 
-dataType='train2017'
-annFile='{}/annotations/instances_{}.json'.format(dataDir,dataType)
-coco=COCO(annFile)
+# trainMaskDir = os.path.join(trainDir,'mask')
+# trainImgDir = os.path.join(trainDir,'image')
+# valImgDir = os.path.join(validateDir,'image')
+# valMaskDir = os.path.join(validateDir,'mask')
+testDir = os.path.join(dstDir, 'test/input')
+resultDir = os.path.join(dstDir, 'result')
 
-catIds = coco.getCatIds(catNms=['tennis racket'])
-imgIds = coco.getImgIds(catIds=catIds)
+dataType = 'train2017'
+annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
+coco = COCO(annFile)
 
-numTrainSamples = 2000
-numValidateSamples = 750
-numTestSamples = 100
+# catIds = coco.getCatIds(catNms=['tennis racket'])
+categoryNames = ['person', 'chair', 'tv']
+# catIds = coco.getCatIds(catNms=categoryNames)
+# imgIds = coco.getImgIds(catIds=catIds)
 
-images = coco.loadImgs(imgIds)
-count = 0
-for img in images:
-    totalmask = None
-    annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
-    anns = coco.loadAnns(annIds)
-    file = img['file_name']
-    for ann in anns:
-        mask = coco.annToMask(ann)
-        mask = np.array(mask)
-        mask = mask * 255
-        totalmask = mask if (totalmask is None) else totalmask + mask
-    maskImg = im.fromarray(totalmask)
-    if (count < numTrainSamples) :
-        shutil.copyfile(os.path.join(train, file), os.path.join(trainImgDir, file))
-        maskImg.save(os.path.join(trainMaskDir, file))
-    elif (count < numTrainSamples + numValidateSamples) :
-        shutil.copyfile(os.path.join(train, file), os.path.join(valImgDir, file))
-        maskImg.save(os.path.join(valMaskDir, file))
-    elif (count < numTrainSamples + numValidateSamples + numTestSamples) :
-        index = count - (numTrainSamples + numValidateSamples); indexString = str(index)
-        if (index < 10) : indexString = '0' + indexString
-        if (index < 100): indexString = '0' + indexString
-        shutil.copyfile(os.path.join(train, file), os.path.join(testDir, indexString + ".jpg"))
-        maskImg.save(os.path.join(resultDir, indexString + ".jpg"))
-    else :
-        break
-    print(maskImg.size)
-    count+=1
+numTrainSamples = 100
+numValidateSamples = 50
+numTestSamples = 10
+
+# images = coco.loadImgs(imgIds)
+
+# Create target file-structure
+categoryEntries = {}
+for category in categoryNames:
+
+    categoryDir = os.path.join(trainDir, category)
+    trainImDir = os.path.join(categoryDir, "image")
+    trainMaskDir = os.path.join(categoryDir, "mask")
+    os.makedirs(trainImDir,exist_ok=True)
+    os.makedirs(trainMaskDir,exist_ok=True)
+
+    categoryDir = os.path.join(validateDir, category)
+    valImDir = os.path.join(categoryDir, "image")
+    valMaskDir = os.path.join(categoryDir, "mask")
+    os.makedirs(valImDir,exist_ok=True)
+    os.makedirs(valMaskDir,exist_ok=True)
+
+    id = coco.getCatIds(catNms=[category])
+    imgIds = coco.getImgIds(catIds=[id[0]])
+    categoryEntries[category] = {'catId': id, 'imgIds': imgIds, 'trainImages': trainImDir, 'trainMasks': trainMaskDir,
+                              'valImages': valImDir, 'valMasks': valMaskDir}
+
+for categoryName in categoryEntries:
+    categoryEntry = categoryEntries[categoryName]
+    imgIds = categoryEntry.get('imgIds')
+    images = coco.loadImgs(imgIds)
+    count = 0
+    for imgPath in images:
+        totalmask = None
+
+        catId = categoryEntry.get('catId')
+        # annIds = coco.getAnnIds(imgIds=img['id'], catIds=[catId], iscrowd=None)
+        annIds = coco.getAnnIds(imgIds=imgPath['id'], catIds=[catId[0]], iscrowd=None)
+        anns = coco.loadAnns(annIds)
+        file = imgPath['file_name']
+        for ann in anns:
+            mask = coco.annToMask(ann)
+            mask = np.array(mask)
+            mask = mask * 255
+            totalmask = mask if (totalmask is None) else totalmask + mask
+            maskImg = im.fromarray(totalmask)
+        if (count < numTrainSamples):
+            shutil.copyfile(os.path.join(train, file), os.path.join(categoryEntry.get('trainImages'), file))
+            maskImg.save(os.path.join(categoryEntry.get('trainMasks'), file))
+        elif (count < numTrainSamples + numValidateSamples):
+            shutil.copyfile(os.path.join(train, file), os.path.join(categoryEntry.get('valImages'), file))
+            maskImg.save(os.path.join(categoryEntry.get('valMasks'), file))
+        # elif (count < numTrainSamples + numValidateSamples + numTestSamples):
+        #     index = count - (numTrainSamples + numValidateSamples);
+        #     indexString = str(index)
+        #     if (index < 10): indexString = '0' + indexString
+        #     if (index < 100): indexString = '0' + indexString
+        #     shutil.copyfile(os.path.join(train, file), os.path.join(testDir, indexString + ".jpg"))
+        #     maskImg.save(os.path.join(resultDir, indexString + ".jpg"))
+        else:
+            break
+        print(maskImg.size)
+        count += 1
